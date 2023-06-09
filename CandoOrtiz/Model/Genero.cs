8c18256ab4da5bac303ca26b8dbc9ae98f79d600
jonas <fsosa@nirsa.com>
2023-06-08 19:53:04 -0500

    first

---
+++ CandoOrtiz/Model/Genero.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace DeberDaw.Model
+{
+    public class Genero
+    {
+        public int Id { set; get; }
+
+        public String? Nombre { set; get; }
+
+        public Boolean Estado { set; get; }
+
+
+
+
+
+    }
+}
---
+++ CandoOrtiz/Model/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace DeberDaw.Model
+{
+    public class Usuario
+    {
+        public int Id { set; get; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public String? NombreUsuario { set; get; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public String? Clave { set; get; }
+
+        [Required]
+        public Boolean Estado { set; get; }
+
+        public int GeneroId { set; get; }
+
+        public Genero? Genero { set; get; }
+
+    }
+}
